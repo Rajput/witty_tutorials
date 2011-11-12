108e770af80d0257e4d3409df1caf02b2a264fc3
Parvinder Rajput <parvinderrajput1990@gmail.com>
2011-11-12 14:14:31 +0530

    Tutorial about printing of Hello added

---
+++ hello/build/source/CMakeFiles/hello.wt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hello.wt.dir/test.C.o"
+  "hello.wt.pdb"
+  "hello.wt"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hello.wt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- hello/source/test.C
+++ hello/source/test.C
@@ -1,6 +1,29 @@
 #include"test.h"
 
+/*
+ * The env argument contains information about the new session, and
+ * the initial request. It must be passed to the WApplication
+ * constructor so it is typically also an argument for your custom
+ * application constructor.
+*/
+WebPage::WebPage(const WEnvironment& env)
+  : WApplication(env)
+{
+  setTitle("Parvinder's Home ");                               // application title
 
+  root()->addWidget(new WText("Parvinder Kumar")); 
+  root()->addWidget(new WText(" Rajput"));
+}
+
+
+WApplication *createApplication(const WEnvironment& env)
+{
+  /*
+   * You could read information from the environment to decide whether
+   * the user has permission to start a new application
+   */
+  return new WebPage(env);
+}
 int main(int argc, char **argv) { 
 
 return WRun(argc, argv, &createApplication);
